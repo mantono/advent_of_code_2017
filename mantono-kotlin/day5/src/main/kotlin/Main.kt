a6b764557cc9eefe73755323f536f90ec2cd1bc7
Anton Österberg <anton.osterberg@gmail.com>
2017-12-14 09:31:05 +0100

    Day 5, part 2.

--- mantono-kotlin/day5/src/main/kotlin/Main.kt
+++ mantono-kotlin/day5/src/main/kotlin/Main.kt
@@ -5,19 +5,25 @@ import java.nio.file.Files
 
 fun main(args: Array<String>)
 {
-	val instructions: MutableList<Short> = Files.readAllLines(File("input").toPath())
+	val instructions: List<Short> = Files.readAllLines(File("input").toPath())
 			.asSequence()
 			.map { it.toShort() }
-			.toMutableList()
+			.toList()
 
-	println(computeInstructions(instructions))
+	println(computeInstructions(instructions.toMutableList()))
+	println(computeInstructions(instructions.toMutableList(), 3))
 }
 
-tailrec fun computeInstructions(instructions: MutableList<Short>, index: Int = 0, steps: Int = 0): Int
+tailrec fun computeInstructions(instructions: MutableList<Short>, decreaseThreshold: Int = Int.MAX_VALUE, index: Int = 0, steps: Int = 0):
+		Int
 {
 	if(index < 0 || index > instructions.lastIndex) return steps
 	val jump: Int = instructions[index].toInt()
 	val goto: Int = index + jump
-	instructions[index]++
-	return computeInstructions(instructions, goto, steps + 1)
+	when(jump >= decreaseThreshold)
+	{
+		true -> instructions[index]--
+		false -> instructions[index]++
+	}
+	return computeInstructions(instructions, decreaseThreshold, goto, steps + 1)
 }
--- mantono-kotlin/day5/src/test/kotlin/CountInstructionTest.kt
+++ mantono-kotlin/day5/src/test/kotlin/CountInstructionTest.kt
@@ -5,10 +5,18 @@ import org.junit.jupiter.api.Test
 class CountInstructionTest
 {
 	@Test
-	fun testCountInstructions()
+	fun testCountInstructionsPart1()
 	{
 		val input: MutableList<Short> = mutableListOf(0, 3, 0, 1, -3)
-		val jumps: Int = computeInstructions(input)
+		val jumps: Int = computeInstructions(input, Int.MAX_VALUE)
 		assertEquals(5, jumps)
+	}
+
+	@Test
+	fun testCountInstructionsPart2()
+	{
+		val input: MutableList<Short> = mutableListOf(0, 3, 0, 1, -3)
+		val jumps: Int = computeInstructions(input, 3)
+		assertEquals(10, jumps)
 	}
 }
